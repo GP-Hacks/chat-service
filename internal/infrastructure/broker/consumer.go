ac7a11fbf3df75554dcfb465b944a1c9a5a2cb8e
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-05-26 10:18:43 +0300

    add consumer and producer broker

---
+++ internal/infrastructure/broker/consumer.go
@@ -0,0 +1,53 @@
+package broker
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/GP-Hacks/chat/internal/models"
+	"github.com/GP-Hacks/chat/internal/services/chat_service"
+	"github.com/segmentio/kafka-go"
+)
+
+type KafkaConsumer struct {
+	reader  *kafka.Reader
+	service *chat_service.ChatService
+}
+
+type messageDto struct {
+	AuthToken string
+	MessageID string
+	Content   string
+}
+
+func NewKafkaConsumer(broker, topic, groupID string) *KafkaConsumer {
+	return &KafkaConsumer{
+		reader: kafka.NewReader(kafka.ReaderConfig{
+			Brokers:  []string{broker},
+			Topic:    topic,
+			GroupID:  groupID,
+			MinBytes: 1e3, // 1KB
+			MaxBytes: 1e6, // 1MB
+		}),
+	}
+}
+
+func (c *KafkaConsumer) Start() {
+	for {
+		m, err := c.reader.ReadMessage(context.Background())
+		if err != nil {
+			continue
+		}
+		var message messageDto
+		if err := json.Unmarshal(m.Value, &message); err != nil {
+			continue
+		}
+
+		msg := models.Message{
+			Content: message.Content,
+			Role:    models.User,
+		}
+
+		go c.service.Ask(context.Background(), message.AuthToken, message.MessageID, &msg)
+	}
+}
---
+++ internal/infrastructure/broker/producer.go
@@ -0,0 +1,80 @@
+package broker
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"github.com/GP-Hacks/chat/internal/models"
+	"github.com/segmentio/kafka-go"
+)
+
+type KafkaProducer struct {
+	writer *kafka.Writer
+}
+
+type respDto struct {
+	Status    string    `json:"status,omitempty"`
+	Error     string    `json:"error,omitempty"`
+	Content   string    `json:"content,omitempty"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+}
+
+func NewKafkaProducer(broker, topic string) *KafkaProducer {
+	return &KafkaProducer{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(broker),
+			Topic:    topic,
+			Balancer: &kafka.LeastBytes{},
+		},
+	}
+}
+
+func (p *KafkaProducer) Send(msg *models.Message) error {
+	dto := respDto{
+		Status:    "success",
+		Content:   msg.Content,
+		CreatedAt: time.Now(), //TODO: replace to real rime
+	}
+	data, err := json.Marshal(dto)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < 3; i++ {
+		err = p.writer.WriteMessages(context.Background(), kafka.Message{
+			Key:   []byte(msg.ID),
+			Value: data,
+		})
+
+		if err == nil {
+			return nil
+		}
+	}
+
+	return err
+}
+
+func (p *KafkaProducer) SendError(id string, err error) error {
+	dto := respDto{
+		Status: "error",
+		Error:  err.Error(),
+	}
+	data, err := json.Marshal(dto)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < 3; i++ {
+		err = p.writer.WriteMessages(context.Background(), kafka.Message{
+			Key:   []byte(id),
+			Value: data,
+		})
+
+		if err == nil {
+			return nil
+		}
+	}
+
+	return err
+}
