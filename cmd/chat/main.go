3b8f31a86ae5790e9ed0966be4cff1373b067f54
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-05-27 20:40:42 +0300

    add main go

--- cmd/chat/main.go
+++ cmd/chat/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"net"
+
+	"github.com/GP-Hacks/chat/internal/config"
+	"github.com/GP-Hacks/chat/internal/servie_provider"
+	"github.com/GP-Hacks/chat/internal/utils/logger"
+	proto "github.com/GP-Hacks/proto/pkg/api/chat"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/reflection"
+)
+
+func main() {
+	config.LoadConfig("./config")
+	logger.SetupLogger()
+	serviceProvider := servie_provider.NewServiceProvider()
+
+	log.Info().Msg("Init app")
+
+	grpcServer := grpc.NewServer(grpc.Creds(insecure.NewCredentials()))
+	reflection.Register(grpcServer)
+
+	proto.RegisterChatServiceServer(grpcServer, serviceProvider.ChatController())
+
+	list, err := net.Listen("tcp", ":"+config.Cfg.Grpc.Port)
+	if err != nil {
+		log.Fatal().Msg("Failed start listen port")
+	}
+
+	err = grpcServer.Serve(list)
+	if err != nil {
+		log.Fatal().Msg("Failed serve grpc")
+	}
+
+	serviceProvider.KafkaConsumer().Start()
+}
