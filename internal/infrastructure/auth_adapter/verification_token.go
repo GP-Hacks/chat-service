1855d4bb2271e1b6b58b7ffbe86c656d0f02b8db
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-06-05 14:51:18 +0300

    add log to auth adapter

--- internal/infrastructure/auth_adapter/verification_token.go
+++ internal/infrastructure/auth_adapter/verification_token.go
@@ -5,14 +5,15 @@ import (
 
 	"github.com/GP-Hacks/chat/internal/services"
 	"github.com/GP-Hacks/proto/pkg/api/auth"
+	"github.com/rs/zerolog/log"
 )
 
 func (a *AuthAdapter) VerifyToken(ctx context.Context, token string) (int64, error) {
 	resp, err := a.client.VerifyAccessToken(ctx, &auth.VerifyAccessTokenRequest{
 		Access: token,
 	})
-
 	if err != nil {
+		log.Debug().Msg(err.Error())
 		return 0, services.InternalServerError
 	}
 
