e6aed12d236b5fe0bdb1ac5b85e8e529e3f3871f
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-06-05 14:52:08 +0300

    upd chat service

--- internal/services/chat_service/ask.go
+++ internal/services/chat_service/ask.go
@@ -2,20 +2,24 @@ package chat_service
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/GP-Hacks/chat/internal/models"
 	"github.com/GP-Hacks/chat/internal/services"
+	"github.com/rs/zerolog/log"
 )
 
 func (s *ChatService) Ask(ctx context.Context, token string, id string, msg *models.Message) {
 	userID, err := s.authAdapter.VerifyToken(ctx, token)
 	if err != nil {
+		log.Error().Msg("failed verify token: " + err.Error())
 		s.brokerProducer.SendError(id, err)
 		return
 	}
 
 	history, err := s.chatRepository.Get(ctx, userID, 5, 0)
 	if err != nil {
+		log.Error().Msg("failed get history: " + err.Error())
 		s.brokerProducer.SendError(id, err)
 		return
 	}
@@ -23,17 +27,19 @@ func (s *ChatService) Ask(ctx context.Context, token string, id string, msg *mod
 	history = append(history, *msg)
 
 	asw, err := s.botAdapter.Chat(ctx, history...)
-	if len(asw)+1 != len(history) {
+	if err != nil {
+		log.Error().Msg(fmt.Sprintf("failed get answer: %v", err))
 		s.brokerProducer.SendError(id, services.InternalServerError)
 		return
 	}
 
 	if err := s.chatRepository.Add(ctx, userID, &asw[len(asw)-1]); err != nil {
+		log.Error().Msg("failed write answer: " + err.Error())
 		s.brokerProducer.SendError(id, err)
 		return
 	}
 
 	if err := s.brokerProducer.Send(id, &asw[len(asw)-1]); err != nil {
-
+		log.Error().Msg("failed send answer to broker: " + err.Error())
 	}
 }
--- internal/services/chat_service/get_history.go
+++ internal/services/chat_service/get_history.go
@@ -4,13 +4,21 @@ import (
 	"context"
 
 	"github.com/GP-Hacks/chat/internal/models"
+	"github.com/rs/zerolog/log"
 )
 
 func (s *ChatService) GetHistory(ctx context.Context, token string, limit, offset int64) ([]models.Message, error) {
 	userID, err := s.authAdapter.VerifyToken(ctx, token)
 	if err != nil {
+		log.Error().Msg("failed verify token: " + err.Error())
 		return nil, err
 	}
 
-	return s.chatRepository.Get(ctx, userID, limit, offset)
+	his, err := s.chatRepository.Get(ctx, userID, limit, offset)
+	if err != nil {
+		log.Error().Msg("failed verify token: " + err.Error())
+		return nil, err
+	}
+
+	return his, nil
 }
