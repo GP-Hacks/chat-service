06ea7e96bb665221fa09db8a822bf299ad5e87fe
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-06-08 14:32:24 +0300

    added save user messages to history

--- internal/services/chat_service/ask.go
+++ internal/services/chat_service/ask.go
@@ -24,22 +24,44 @@ func (s *ChatService) Ask(ctx context.Context, token string, id string, msg *mod
 		return
 	}
 
+	if err := s.chatRepository.Add(ctx, userID, msg); err != nil {
+		log.Error().Msg("failed question answer: " + err.Error())
+		s.brokerProducer.SendError(id, err)
+		return
+	}
+
 	history = append(history, *msg)
 
 	asw, err := s.botAdapter.Chat(ctx, history...)
 	if err != nil {
 		log.Error().Msg(fmt.Sprintf("failed get answer: %v", err))
+		if err := s.chatRepository.Add(ctx, userID, &models.Message{Content: "internal server error", Role: models.Bot}); err != nil {
+			log.Error().Msg("failed question answer: " + err.Error())
+			s.brokerProducer.SendError(id, err)
+			return
+		}
+
 		s.brokerProducer.SendError(id, services.InternalServerError)
 		return
 	}
 
 	if err := s.chatRepository.Add(ctx, userID, &asw[len(asw)-1]); err != nil {
 		log.Error().Msg("failed write answer: " + err.Error())
+		if err := s.chatRepository.Add(ctx, userID, &models.Message{Content: "internal server error", Role: models.Bot}); err != nil {
+			log.Error().Msg("failed question answer: " + err.Error())
+			s.brokerProducer.SendError(id, err)
+			return
+		}
 		s.brokerProducer.SendError(id, err)
 		return
 	}
 
 	if err := s.brokerProducer.Send(id, &asw[len(asw)-1]); err != nil {
+		if err := s.chatRepository.Add(ctx, userID, &models.Message{Content: "internal server error", Role: models.Bot}); err != nil {
+			log.Error().Msg("failed question answer: " + err.Error())
+			s.brokerProducer.SendError(id, err)
+			return
+		}
 		log.Error().Msg("failed send answer to broker: " + err.Error())
 	}
 }
